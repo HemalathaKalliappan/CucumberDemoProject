f509396588fd89742a76e5620c0f9621c7db04bc
khemalatha <khemalatha@HTSPC91.hts.com>
2019-06-10 12:46:18 +0530

    commit

--- CucumberPro/src/test/java/runner/TestNGRunner.java
+++ CucumberPro/src/test/java/runner/TestNGRunner.java
@@ -1,7 +1,10 @@
 package runner;
 
+import org.junit.runner.RunWith;
+
 import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
+
 
 @CucumberOptions(
 		features="src/test/resources/DatatableUsingMap.feature",
@@ -11,6 +14,7 @@ import cucumber.api.testng.AbstractTestNGCucumberTests;
 		dryRun = false
 
 		)
+
 public class TestNGRunner extends AbstractTestNGCucumberTests {
 
 }
